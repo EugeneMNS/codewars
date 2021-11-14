8ffc50dbff81c3e237ab6bcb67793c5eee821fe7
EugeneMNS <70910236+EugeneMNS@users.noreply.github.com>
2021-11-14 15:28:29 +0300

    Update codewarsSolutions.js

--- codewarsSolutions.js
+++ codewarsSolutions.js
@@ -1,3 +1,15 @@
 var a = "code";
 var b = "wa.rs";
 var name = a + b;
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old < 18) {
+    return "drink coke";
+  } else if (old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
